d888e4698ee26141d920f57e9f91ba6d3f3a6a69
somichev-dev <84933205+somichev-dev@users.noreply.github.com>
2024-04-20 21:33:27 +0300

    Finalise weapon table GUI and start adding recipes

--- src/main/kotlin/recipes/WeaponTableRecipes.kt
+++ src/main/kotlin/recipes/WeaponTableRecipes.kt
@@ -6,15 +6,31 @@ import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 enum class WeaponTableRecipes(
-    base: ItemStack,
-    modifier: ItemStack,
-    extra: ItemStack,
-    result: ItemStack
+    val base: (ItemStack) -> Boolean,
+    val modifier: (ItemStack) -> Boolean,
+    val metal: (ItemStack) -> Boolean,
+    val fuel: (ItemStack) -> Boolean = {item -> item.type == Material.BLAZE_ROD},
+    val result: (ItemStack, ItemStack, ItemStack, ItemStack) -> ItemStack
 ) {
     LIGHT_RIFLE(
-        base = ItemStack(Material.BOW),
-        extra = RifledBarrelItem.getItem(),
-        modifier = ItemStack(Material.IRON_INGOT, 6),
-        result = LightRifleGunItem.getItem()
-    )
+        base = {item -> item.type == Material.BOW},
+        modifier = {item -> RifledBarrelItem.compare(item) },
+        metal = {item -> item.amount >= 6 && item.type == Material.IRON_INGOT },
+        result = {weapon, modifier, metal, fuel ->
+            weapon.amount -= 1
+            modifier.amount -= 1
+            metal.amount -= 6
+            fuel.amount -= 1
+            LightRifleGunItem.getItem() }
+    );
+    companion object{
+        fun getRecipe(weapon: ItemStack, metal: ItemStack, modifier: ItemStack, fuel: ItemStack): ItemStack? {
+            val targetRecipe = WeaponTableRecipes.entries
+                .filter{it.base(weapon)}
+                .filter{it.metal(metal)}
+                .filter{it.fuel(fuel)}
+                .firstOrNull { it.modifier(modifier) } ?: return null
+            return targetRecipe.result(weapon, modifier, metal, fuel)
+        }
+    }
 }
