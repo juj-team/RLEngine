d2bed10ae6d81b39ff2a26fb7092aaad6b22d691
somichev-dev <84933205+somichev-dev@users.noreply.github.com>
2024-05-09 17:49:51 +0300

    temperatures tweak

--- src/main/kotlin/climate/calculation/BiomeWarmness.kt
+++ src/main/kotlin/climate/calculation/BiomeWarmness.kt
@@ -16,11 +16,11 @@ object BiomeWarmness : TemperatureCalculator {
             Biome.RIVER to 15.0,
             // cursed biomes
             Biome.BADLANDS to 35.0,
-            Biome.ERODED_BADLANDS to 42.0,
+            Biome.ERODED_BADLANDS to 35.0,
             Biome.COLD_OCEAN to -65.0,
             Biome.DARK_FOREST to 20.0,
             Biome.DEEP_OCEAN to -20.0,
-            Biome.DESERT to 40.0,
+            Biome.DESERT to 32.0,
             Biome.FROZEN_RIVER to -65.0,
             Biome.JUNGLE to 37.0,
             Biome.OCEAN to 10.0,
--- src/main/kotlin/climate/calculation/EnvironmentWarmness.kt
+++
@@ -1,9 +0,0 @@
-package climate.calculation
-
-import org.bukkit.entity.Player
-
-object EnvironmentWarmness : TemperatureCalculator {
-    override fun calculate(player: Player): Double {
-        return 0.0
-    }
-}
